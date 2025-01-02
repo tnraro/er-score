6b01e7e10a4873c5eb6bc7d15a930425d3a03070
tnraro <deokji53@gmail.com>
2025-01-02 14:47:27 +0900

    fix score style

--- src/lib/components/ui/score/score.ts
+++ src/lib/components/ui/score/score.ts
@@ -2,7 +2,7 @@ import { tv, type VariantProps } from "tailwind-variants";
 
 export type ScoreVariants = VariantProps<typeof score>;
 export const score = tv({
-  base: "w-10 inline-block rounded-full px-1 text-center font-extrabold",
+  base: "w-10 inline-flex items-center justify-center rounded-full px-1 text-center font-extrabold",
   variants: {
     score: {
       [1]: "bg-green-200/50 text-green-900",
