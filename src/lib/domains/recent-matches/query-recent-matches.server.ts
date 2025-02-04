f489065cba5698b2937de9964111c7a06c5bc7e5
tnraro <deokji53@gmail.com>
2025-02-05 02:41:15 +0900

    modify number of recent matches fetched at once from 12 to 6

--- src/lib/domains/recent-matches/query-recent-matches.server.ts
+++ src/lib/domains/recent-matches/query-recent-matches.server.ts
@@ -2,5 +2,5 @@ import type { Database } from "$lib/server/db/client";
 import { selectRecentMatches } from "./select-recent-matches.server";
 
 export async function queryRecentMatches(db: Database, userId: number) {
-  return selectRecentMatches(db, userId, 12);
+  return selectRecentMatches(db, userId, 6);
 }
