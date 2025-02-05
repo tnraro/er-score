20dab0b49374610fdc919531c8a2c5f2996b1a18
tnraro <deokji53@gmail.com>
2025-02-06 00:08:02 +0900

    fix .

--- src/routes/users/[username]/+page.server.ts
+++ src/routes/users/[username]/+page.server.ts
@@ -2,42 +2,56 @@ import { mutateMatches } from "$lib/domains/matches/mutate-matches.server";
 import { mutateRecentMatches } from "$lib/domains/recent-matches/mutate-recent-matches.server";
 import { selectRecentMatches } from "$lib/domains/recent-matches/select-recent-matches.server.js";
 import { selectUserStats } from "$lib/domains/user-stats/select-user-stats.js";
+import { queryUser } from "$lib/domains/user/query-user.server.js";
 import { updateUserUpdatedAt } from "$lib/server/db/models/update-user-updated-at";
+import { error } from "@sveltejs/kit";
 
-export async function load({ params: { username }, depends, parent }) {
+export async function load({ params: { username }, depends }) {
   depends(`/users/${username}`);
-  const { user } = await parent();
-  const staleStatsPromise = selectUserStats(user.id);
-  const staleMatchesPromise = selectRecentMatches(user.id);
+  try {
+    const user = await queryUser(username);
+    const staleStatsPromise = selectUserStats(user.id);
+    const staleMatchesPromise = selectRecentMatches(user.id);
 
-  const freshPromise = update();
+    const freshPromise = update();
 
-  return {
-    staleStatsPromise,
-    staleMatchesPromise,
-    freshPromise,
-  };
-  async function update() {
-    if (user.updatedAt != null && Date.now() - user.updatedAt.getTime() <= 1000 /** 1 second */)
-      return Promise.resolve(null);
-
-    const updatedMatchId = await mutateMatches(user.id, {
-      matchId: user.updatedMatchId,
-      pages: 10,
-    });
-    const hasNewMatches = updatedMatchId != null && updatedMatchId != user.updatedMatchId;
-    if (!hasNewMatches) return Promise.resolve(null);
-    await updateUserUpdatedAt(user.id, updatedMatchId);
-    const staleMatches = await staleMatchesPromise;
-    const { changed } = await mutateRecentMatches(user.id, staleMatches);
-    if (!changed) return Promise.resolve(null);
-    const [stats, matches] = await Promise.allSettled([
-      selectUserStats(user.id),
-      selectRecentMatches(user.id),
-    ]);
     return {
-      stats,
-      matches,
+      user,
+      staleStatsPromise,
+      staleMatchesPromise,
+      freshPromise,
     };
+    async function update() {
+      if (user.updatedAt != null && Date.now() - user.updatedAt.getTime() <= 1000 /** 1 second */)
+        return Promise.resolve(null);
+
+      const updatedMatchId = await mutateMatches(user.id, {
+        matchId: user.updatedMatchId,
+        pages: 10,
+      });
+      const hasNewMatches = updatedMatchId != null && updatedMatchId != user.updatedMatchId;
+      if (!hasNewMatches) return Promise.resolve(null);
+      await updateUserUpdatedAt(user.id, updatedMatchId);
+      const staleMatches = await staleMatchesPromise;
+      const { changed } = await mutateRecentMatches(user.id, staleMatches);
+      if (!changed) return Promise.resolve(null);
+      const [stats, matches] = await Promise.allSettled([
+        selectUserStats(user.id),
+        selectRecentMatches(user.id),
+      ]);
+      return {
+        stats,
+        matches,
+      };
+    }
+  } catch (e) {
+    if (e instanceof Response) {
+      if (e.status === 404) {
+        error(e.status, "찾을 수 없습니다");
+      }
+      console.error(e);
+      error(e.status, await e.text());
+    }
+    throw e;
   }
 }
