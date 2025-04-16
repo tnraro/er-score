a101c974fdbdf8c5037748f91c4927461dd10fbe
tnraro <deokji53@gmail.com>
2025-04-16 17:33:26 +0900

    fix request bug

--- src/_worker.js
+++ src/_worker.js
@@ -3,7 +3,7 @@ const worker = {
     const baseUrl = new URL(req.url);
     const path = baseUrl.href.slice(baseUrl.origin.length);
     const url = new URL(path, env.APP_HOST);
-    const res = await fetch(url);
+    const res = await fetch(url, req);
     const headers = new Headers(res.headers);
     headers.delete("x-served-by");
     return new Response(res.body, {
