5155ac9b85c8a9538a9887839610556a1fd83b32
tnraro <deokji53@gmail.com>
2025-04-23 14:56:17 +0900

    feat(api): split two priority for api queue

--- src/lib/features/er-api/api-queue.ts
+++ src/lib/features/er-api/api-queue.ts
@@ -1,11 +1,24 @@
 import { sleep } from "$lib/utils/time/sleep";
 
+type Queue = {
+  task: () => Promise<any>;
+  resolve: (value?: any) => void;
+  reject: (reason?: any) => void;
+}[];
+
+export enum ApiQueuePriority {
+  Main,
+  Sub,
+}
+
 export class ApiQueue {
   #queue: {
-    task: () => Promise<any>;
-    resolve: (value?: any) => void;
-    reject: (reason?: any) => void;
-  }[] = [];
+    [ApiQueuePriority.Main]: Queue;
+    [ApiQueuePriority.Sub]: Queue;
+  } = {
+    [ApiQueuePriority.Main]: [],
+    [ApiQueuePriority.Sub]: [],
+  };
   #isRunning = false;
   #delay;
   get delay() {
@@ -14,9 +27,9 @@ export class ApiQueue {
   constructor(delay: number) {
     this.#delay = delay;
   }
-  add<T>(task: () => Promise<T>): Promise<T> {
+  add<T>(task: () => Promise<T>, priority: ApiQueuePriority = ApiQueuePriority.Main): Promise<T> {
     const { resolve, reject, promise } = Promise.withResolvers<T>();
-    this.#queue.push({
+    this.#queue[priority].push({
       task,
       resolve,
       reject,
@@ -28,12 +41,18 @@ export class ApiQueue {
     if (this.#isRunning) return;
     this.#isRunning = true;
     while (true) {
-      const value = this.#queue.shift();
+      const value = this.#shift();
       if (value == null) break;
       const { task, resolve, reject } = value;
       task().then(resolve).catch(reject);
       await sleep(this.#delay);
     }
     this.#isRunning = false;
+  }
+  #shift() {
+    if (this.#queue[ApiQueuePriority.Main].length > 0) {
+      return this.#queue[ApiQueuePriority.Main].shift();
+    }
+    return this.#queue[ApiQueuePriority.Sub].shift();
   }
 }
